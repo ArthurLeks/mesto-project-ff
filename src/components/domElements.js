8df15514149f173c1831ef84c01db9d9f773cb7b
ArthurLeks <114483838+ArthurLeks@users.noreply.github.com>
2023-12-01 17:09:52 +0300

    1 - Модуль (5 пр,6 пр)
    1 Начальный вариант (до проверки)

---
+++ src/components/domElements.js
@@ -0,0 +1,14 @@
+export const cardContainer = document.querySelector('.places__list'); 
+export const imagePopup = document.querySelector('.popup_type_image');
+export const profileEditPopup = document.querySelector('.popup_type_edit');
+export const addCardPopup = document.querySelector('.popup_type_new-card');
+export const popups = [
+    {
+        openBtn: document.querySelector('.profile__edit-button'),
+        popupWnd: profileEditPopup,
+    },
+    {
+        openBtn: document.querySelector('.profile__add-button'),
+        popupWnd: addCardPopup,
+    },
+]
---
+++ src/index.js
@@ -0,0 +1,27 @@
+import './styles/index.css';
+import {createCard, initialCards} from './components/cards.js'
+import {cardContainer, popups} from './components/domElements.js'
+import {closeOpenedPopupByOverlay, openPopup} from './components/modal.js'
+
+
+function initalizeCards(cards) {
+   
+    cards.forEach(item => {
+        const cardElement = createCard(item.name, item.link);
+        cardContainer.appendChild(cardElement);
+    });
+}
+
+function initPopups(popups){
+    document.addEventListener('click', closeOpenedPopupByOverlay);
+    popups.forEach(item => {
+        const openBtn = item.openBtn;
+        const popupWindow = item.popupWnd;
+        openBtn.addEventListener('click', event => openPopup(popupWindow));
+    });
+   
+}
+
+initalizeCards(initialCards);
+initPopups(popups);
+
